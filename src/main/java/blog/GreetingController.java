8abefed9d745a368e14d6ffa1811ccac1da433f4
Valentin Ramecourt <valentin.ramecourt@etudiant.univ-lille1.fr>
2016-12-05 10:44:39 +0100

    Add WebSocket

--- src/main/java/blog/GreetingController.java
+++ src/main/java/blog/GreetingController.java
@@ -1,17 +1,18 @@
-package hello;
+package blog;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+
+    @MessageMapping("/blog")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(Message message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
     }
 
 }
